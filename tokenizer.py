83d14f2544ed30842205bbb8e01c9cc3a38d0de7
Laurens Berthold <lberthold@gmx.net>
2022-11-18 12:56:31 +0100

    Detect sentences

--- tokenizer.py
+++ tokenizer.py
@@ -48,10 +48,15 @@ def strip_brackets(segment: str, start_char: str="(", end_char: str=")") -> str:
     return non_bracket_text
 
 
-def tokenize(segment: str) -> List[str]:
-    segment = segment.replace("\t", " ").strip()
+def prepare_segment_for_tokenization(segment: str) -> str:
+    """
+    Apply various replacements, strip brackets
+    """
+    # transcribers use commas more like enumerations, so do not use them
+    segment = segment.replace(",", " ")
     # combine multiple whitespaces
     segment = _RE_COMBINE_WHITESPACE.sub(" ", segment)
+    segment = segment.strip()
     # replace some text with a dot
     for regexp in REPLACE_TO_DOTS:
         segment = regexp.sub(". ", segment)
@@ -60,8 +65,51 @@ def tokenize(segment: str) -> List[str]:
                                  ["[", "]"],
                                  ["{", "}"]]:
         segment = strip_brackets(segment, start_char=start_char, end_char=end_char)
-    tokens = [token.strip() for token in segment.split(" ")]
+    return segment
+
+
+def tokenize(segment: str) -> List[str]:
+    """
+    Prepares segment and tokenizes. A dot will be preserved as a separate token.
+    """
+    segment = prepare_segment_for_tokenization(segment)
+    tokens = []
+    current_token = ""
+    # iterate char-wise for simple algorithm
+    for char in segment:
+        if char == " ":
+            if len(current_token) > 0:
+                tokens += [current_token]
+                current_token = ""
+        elif char == ".":
+            assert len(current_token) > 0
+            tokens += [current_token, "."]
+            current_token = ""
+        else:
+            current_token += char
+    if len(current_token) > 0:
+        tokens += [current_token]
     return [token for token in tokens if keep_token(token)]
+
+
+def tokens_to_sentences(tokens: List[str]) -> List[List[str]]:
+    """
+    Finds sentence stops in token list.
+    :param tokens: List of tokens
+    :return: Creates a list of sentences. Each sentence is a list of tokens
+    """
+    sentences = []
+    current_sentence = []
+    for token in tokens:
+        if token == ".":
+            assert len(current_sentence) > 0
+            sentences += [current_sentence]
+            current_sentence = []
+        else:
+            current_sentence += [token]
+    if len(current_sentence) > 0:
+        sentences += [current_sentence]
+    return sentences
 
 
 def build_vocabulary(segments: List[str]) -> Dict[str, int]:
