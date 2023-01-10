e47b86b444934c17329289624305b92d4d2b9456
Laurens Berthold <lberthold@gmx.net>
2023-01-10 13:47:03 +0100

    Convert and filter RF-Score

--- data_reader.py
+++ data_reader.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import warnings
 from typing import List
 
 
@@ -23,6 +25,22 @@ def keep_line(line: str) -> bool:
     return True
 
 
+def filter_and_convert_non_numeric_scores(df: pd.DataFrame, colname: str = "RF-Score") -> pd.DataFrame:
+    if df.dtypes[colname] == np.dtype("int64"):
+        return df
+    elif df.dtypes[colname] in (np.dtype("str"), np.dtype("object")):
+        mask = [str(score).isnumeric() for score in df[colname].values]
+        if sum(mask) < len(df):
+            warnings.warn(f"Dropping {len(df) - sum(mask)} rows due to non-integer values in {colname}")
+        df = df.loc[mask]
+        pd.set_option('mode.chained_assignment', None)
+        df[colname] = df[colname].astype(int)
+        pd.set_option('mode.chained_assignment', 'warn')
+        return df
+    else:
+        raise TypeError(f"Unknown type for column {colname}: {df.dtypes[colname]}")
+
+
 def strip_segment(segment: str) -> List[str]:
     """
     Strip empty lines and metadata
@@ -34,6 +52,7 @@ def strip_segment(segment: str) -> List[str]:
 
 def read_full_dataset(path: str) -> pd.DataFrame:
     df = pd.read_excel(path)
+    df = filter_and_convert_non_numeric_scores(df)
     df["Segment_preproc"] = [strip_segment(segment) for segment in df["Segment"]]
     df[["Patient", "Session", "Dokument_Residual"]] = df["Dokumentname"].str.split("_", expand=True)
     if not all(valid := df["Patient"].str.match(pat="^[a-zA-Z0-9öäüßÖÄÜ]+$", case=False)):
